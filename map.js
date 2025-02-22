ddd14923b084090d42f4da8be4bf9d716cd542c3
t2liang <t2liang@ucsd.edu>
2025-02-21 21:42:06 -0800

    Update map.js

--- map.js
+++ map.js
@@ -7,4 +7,37 @@ const map = new mapboxgl.Map({
     zoom: 12, // Initial zoom level
     minZoom: 5, // Minimum allowed zoom
     maxZoom: 18 // Maximum allowed zoom
-  });
+});
+
+map.on('load', () => { 
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+      });
+    map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+    'line-color': '#32D400',  // A bright green using hex code
+    'line-width': 5,          // Thicker lines
+    'line-opacity': 0.6       // Slightly less transparent
+    }
+    });
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+      });
+    map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: {
+    'line-color': '#32D400',  // A bright green using hex code
+    'line-width': 5,          // Thicker lines
+    'line-opacity': 0.6       // Slightly less transparent
+    }
+    });
+});
+  
+
